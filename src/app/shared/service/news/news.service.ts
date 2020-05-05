55ffa1b0ea34bc5480264a464c581d0a5b2d79c0
cwittmann <christofwittmann@web.de>
2020-05-05 17:45:16 +0200

    feat: layout

--- src/app/shared/service/news/news.service.ts
+++ src/app/shared/service/news/news.service.ts
@@ -13,7 +13,7 @@ export class NewsService {
     private databaseService: DatabaseService
   ) {
     databaseService.onInitialized.subscribe(async () => {
-      // await this.getNewsFromApi();
+      //await this.getNewsFromApi();
       this.getNewsFromDB();
     });
   }
@@ -21,7 +21,7 @@ export class NewsService {
   async getNewsFromApi() {
     let data = await this.httpClient
       .get(
-        `https://newsapi.org/v2/everything?q=nuremberg&lang=en&pageSize=100&apiKey=${this.apiKey}`
+        `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=${this.apiKey}`
       )
       .toPromise();
 
